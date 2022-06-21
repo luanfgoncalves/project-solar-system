5cfccb76aaf36c12483903bd0211a936223c2594
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-21 15:02:05 -0300

    FEAT - arquivo e componente do Header

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1> Sistema Solar </h1>
+      </section>
+    );
+  }
+}
+
+export default Header;
