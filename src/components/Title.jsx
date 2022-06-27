99ad1084c00d9b8659766b203f6fcf739dbb2ffc
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-27 02:14:25 -0300

    FEAT - Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
+    const { headline } = this.props;
     return (
-      <section>| Title |</section>
+      <h2>{headline}</h2>
     );
   }
 }
+
+Title.propTypes = {
+  headline: propTypes.string.isRequired,
+};
 
 export default Title;
