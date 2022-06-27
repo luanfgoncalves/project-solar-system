4c87994a7c4dace773a257ba7cb5859eb7e660b0
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-27 02:52:29 -0300

    FEAT - renderização do componente Title dentro do compotente SolarSystem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
-import PlanetCard from './components/PlanetCard';
+// import PlanetCard from './components/PlanetCard';
+// import Missions from './components/Missions';
+// import MissionCard from './componets/MissionCard';
 
 class App extends React.Component {
   render() {
