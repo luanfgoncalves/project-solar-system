fa20ed0d31dbda19967f607f211835a44970a71f
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-27 17:23:44 -0300

    FEAT: Melhorias em Missions.jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
-import MissionCard from './components/MissionCard';
 
 class App extends React.Component {
   render() {
@@ -11,7 +10,6 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
-        <MissionCard />
       </body>
     );
   }
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missionsData from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div testid="missions">
         <Title headline="Missões" />
+        <div>
+          {missionsData.map((element) => (<MissionCard
+            key={ element.name }
+            name={ element.name }
+            year={ element.year }
+            country={ element.country }
+            destination={ element.destination }
+          />))}
+        </div>
       </div>
     );
   }
