2ac88976aeafbd57aee4420b317b4e2628942e30
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-27 16:56:38 -0300

    FEAT: componente MissionCard - Ainda incompleto

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 class MissionCard extends React.Component {
   render() {
+    const { name } = this.props;
+    const { year } = this.props;
+    const { country } = this.props;
+    const { destination } = this.props;
     return (
-      <section>| MissionCard |</section>
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{ name }</p>
+        <p data-testid="mission-year">{ year }</p>
+        <p data-testid="mission-country">{ country }</p>
+        <p data-testid="mission-destination">{ destination }</p>
+      </div>
     );
   }
 }
+
+MissionCard.propTypes = {
+  name: propTypes.string.isRequired,
+  year: propTypes.string.isRequired,
+  country: propTypes.string.isRequired,
+  destination: propTypes.string.isRequired,
+};
 
 export default MissionCard;
